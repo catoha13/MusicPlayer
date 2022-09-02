49755728dffb32d1204b5cd1754574f10af7f45c
Artom Prishchepov <catoha@gmail.com>
2022-09-02 15:59:18 +0300

    Initial Commit

---
+++ MusicPlayer(testTask)/MusicPlayer_testTask_App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayer_testTask_App.swift
+//  MusicPlayer(testTask)
+//
+//  Created by Артём on 2.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayer_testTask_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
